9b1ee6cd53666576c57aeeb820b9ee2a5fab0db1
Musa Issa <musaissa004@gmail.com>
2022-10-19 19:20:44 +0100

    test for input placeholder

--- src/Input/Input.jsx
+++ src/Input/Input.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 const Input = () => {
   return (
-    <div>
-      Input
-      <form >
-        <input type="text" name="Task" />
-</form>
-    </div>
-  )
+		<div>
+			Input
+			<form>
+				<label>
+					Task:
+					<input type="text" placeholder="Please Enter your Task" />
+				</label>
+			</form>
+		</div>
+	);
 }
 
 export default Input
--- src/Input/Input.test.jsx
+++ src/Input/Input.test.jsx
@@ -1,9 +1,20 @@
-import {render, screen} from "@testing-library/react";
-import "@testing-library/jest-dom";
+import {render, renderHook, screen} from "@testing-library/react";
 import Input from "./Input.jsx";
-test('should have a form', () => { second })
+
+test('should have a submit button', () => {
+	render(<Input/>)
+	const labelElement = screen.getByLabelText('Task:')
+	expect(labelElement).toBeInTheDocument();
+});
 test("Should have a task input", () => {
-	render(<Input />);
-	const inputElement = screen.getByRole('textbox');
+	render(<Input name='Task'/>);
+	const inputElement = screen.getByRole("textbox", { name: /task/i });
 	expect(inputElement).toBeInTheDocument();
 });
+
+
+test('should have a placeholder', () => {
+	render(<Input />)
+	const placeholder = screen.getByPlaceholderText('Please Enter your Task')
+	expect(placeholder).toBeInTheDocument()
+})
