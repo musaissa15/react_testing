068b332a115fbb66cdfc48128575b7714fb85a8c
Musa Imad Issa <musaissa004@gmail.com>
2022-10-23 12:55:43 +0100

    Revert "Finish input testing"
    This reverts commit f8f19e1c057248976b5690d7488bb7ff02c3ac91.

--- src/Input/Input.jsx
+++ src/Input/Input.jsx
@@ -1,31 +1,18 @@
 import React, { useState } from "react";
 // import { useState } from 'react'
 const Input = () => {
-    const [task, setTask] = useState('');
-    const [list, setlist] = useState([])
-    const handleChange = (e) => {
-        setTask(e.target.value);
-        console.log(e.target.value);
-    };
-    
-    const addTask = () => {
-        console.log(task)
-    }
-
-    return (
-        <div>
-            <input
-                type='text'
-                placeholder='Please Enter your Task'
-                onChange={(e) => {
-                    handleChange(e)
-                }}
-            />
-            <button type="submit"
-            onClick={addTask}
-            > Add </button>
-    </div>
-  )
+  return (
+		<div>
+			Input
+			<form>
+				<label>
+					Task:
+					<input type="text" placeholder="Please Enter your Task" />
+        </label>
+        <button type="submit"> Add </button>
+			</form>
+		</div>
+	);
 }
 
 export default Input
