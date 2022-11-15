1a04025bda1431f2e586b284f2d2d0045431f206
Musa Imad Issa <musaissa004@gmail.com>
2022-11-15 20:50:15 +0000

    Delete code from list file

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import Todo from "../Todo/Todo.jsx";
 const List = ({ todos, removeTodo, toggleComplete }) => {
 	return (
-		<div className="List">
+		<div>
 			List
 			<ul>
-				{todos.map((t) => {
-					return (
-						<li>
-							<Todo
-								key={t.id}
-								t={t}
-								removeTodo={removeTodo}
-								toggleComplete={toggleComplete}
-							/>
-						</li>
-					);
-				})}
+				<div className="List">
+					{todos.map((t) => {
+						return (
+							<li>
+								<Todo
+									key={t.id}
+									t={t}
+									removeTodo={removeTodo}
+									toggleComplete={toggleComplete}
+								/>
+							</li>
+						);
+					})}
+				</div>
 			</ul>
 		</div>
 	);
---
+++ src/Components/Todo/Todo.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Todo from "./Todo";
+
+describe("", () => {
+	test("should ", () => {
+		render(<Todo />);
+	});
+});
